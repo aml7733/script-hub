350d9f4126a985ec86fc09d99985bf2999fce244
Adam <lucia.adam@gmail.com>
2017-09-26 02:08:30 -0400

    add users to db and add devise sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :scripts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'scripts#index'
 end
